bbfe9f99dba339c058b19b6386e89d070580609e
Takashi KANNAN <takashi.kannan@unity3d.com>
2020-03-03 17:46:32 +0900

    define ISignaling

---
+++ Assets/Scripts/ISignaling.cs
@@ -0,0 +1,17 @@
+﻿using Unity.WebRTC;
+
+namespace Unity.RenderStreaming
+{
+    public delegate void OnOfferEventHandler(OfferResData offerResData);
+    public delegate void OnIceCandidateEventHandler(CandidateResData candidateResData);
+
+    public interface ISignaling
+    {
+        void Start();
+        void Stop();
+        void SendCandidate(string connectionId, RTCIceCandidate candidate);
+        void SendAnswer(string connectionId, RTCPeerConnection peerConnection);
+        event OnOfferEventHandler OnOffer;
+        event OnIceCandidateEventHandler OnIceCandidate;
+    }
+}
